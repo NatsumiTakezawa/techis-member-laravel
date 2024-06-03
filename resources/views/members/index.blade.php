8789a8773ecf42231eb03bdc3ae0f833d1d96112
NatsumiTakezawa <164612500+NatsumiTakezawa@users.noreply.github.com>
2024-06-03 23:01:58 +0900

    CSS

--- resources/views/members/index.blade.php
+++ resources/views/members/index.blade.php
@@ -3,12 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSSを当てたいときはビートで書く。その後ターミナルでnpm run dev を入力 -->
+    @vite(['resources/css/app.css'])
     <title>会員一覧画面</title>
     
 </head>
-<body>
-<a href="{{ route('members.create') }}"> >>登録 </a>
-<table border="1" style="border-collapse: collapse">
+<body class="container">
+  <div>
+<a href="{{ route('members.create') }}" class="create-link"> >>登録 </a>
+<table border="1" style="border-collapse: collapse" class="user-table">
   <thead>
     <tr>
       <th>名前</th>
@@ -23,11 +26,12 @@
       <td>{{ $member->name }}</td>
       <td>{{ $member->phone }}</td>
       <td>{{ $member->email }}</td>
-      <td><a href="{{ route('members.edit', ['id' => $member->id]) }}">>>編集</a></td>
+      <td><a href="{{ route('members.edit', ['id' => $member->id]) }}" class="edit-link"> >>編集 </a></td>
       <!-- <td> >>編集 ({{ route('members.edit', ['id' => $member->id]) }})</td> -->
     </tr>
     @endforeach
   </tbody>
 </table>
+</div>
 </body>
 </html>
